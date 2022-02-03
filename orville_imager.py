b181871ebedad0d7b1576c277f6465d07d77955e
cdilullo <cdilullo@unm.edu>
2022-02-03 08:41:25 -0500

    Added a MatrixOp module to the Orville imager which should compute the average correlation matrix to help quantify the overall state of the station.

--- orville_imager.py
+++ orville_imager.py
@@ -575,6 +575,99 @@ class BaselineOp(object):
                                           'process_time': process_time,})
         self.log.info("BaselineOp - Done")
 
+class MatrixOp(object):
+    def __init__(self, log, iring, base_dir=os.getcwd(), core=-1, gpu=-1):
+        self.log = log
+        self.iring = iring
+        self.output_dir = os.path.join(base_dir, 'matrices')
+        self.core = core
+        self.gpu = gpu
+        
+        if not os.path.exists(base_dir):
+            os.mkdir(base_dir)
+        if not os.path.exists(self.output_dir):
+            os.mkdir(self.output_dir)
+            
+        self.bind_proclog = ProcLog(type(self).__name__+"/bind")
+        self.in_proclog   = ProcLog(type(self).__name__+"/in")
+        self.size_proclog = ProcLog(type(self).__name__+"/size")
+        self.sequence_proclog = ProcLog(type(self).__name__+"/sequence0")
+        self.perf_proclog = ProcLog(type(self).__name__+"/perf")
+        
+        self.in_proclog.update({'nring':1, 'ring0':self.iring.name})
+        
+        self.station = STATION
+
+    def main(self):
+        cpu_affinity.set_core(self.core)
+        if self.gpu != -1:
+            BFSetGPU(self.gpu)
+        self.bind_proclog.update({'ncore': 1, 
+                                  'core0': cpu_affinity.get_core(),
+                                  'ngpu': 1,
+                                  'gpu0': BFGetGPU(),})
+
+        for iseq in self.iring.read(guarantee=True):
+            ihdr = json.loads(iseq.header.tostring())
+            
+            self.sequence_proclog.update(ihdr)
+            self.log.info('MatrixOp: Config - %s', ihdr)
+            
+            # Setup the ring metadata and gulp sizes
+            chan0  = ihdr['chan0']
+            nchan  = ihdr['nchan']
+            nbl    = ihdr['nbl']
+            nstand = int(numpy.sqrt(8*nbl+1)-1)//2
+            npol   = ihdr['npol']
+            navg   = ihdr['navg']
+            time_tag0 = iseq.time_tag
+            time_tag  = time_tag0
+            igulp_size = nstand*(nstand+1)//2*nchan*npol*npol*8
+            ishape = (nstand*(nstand+1)//2,nchan,npol,npol)
+            self.iring.resize(igulp_size, igulp_size*10)
+
+            intCount = 0
+            prev_time = time.time()
+            for ispan in iseq.read(igulp_size):
+                if ispan.size < igulp.size:
+                    continue #Ignore file gulp
+                curr_time = time.time()
+                acquire_time = curr_time - prev_time
+                prev_time = curr_time
+
+                ##Set up and load
+                idata = ispan.data_view(numpy.complex64).reshape(ishape)
+
+                ##Calculate the frequency averaged correlation matrix
+                corr = idata * idata.conj() / (numpy.abs(idata)**2)
+                corr = numpy.mean(corr, axis=1)
+                corr = corr.reshape((nstand, nstand, corr.shape[1], corr.shape[2]))
+
+                #Save
+                ### Timetag stuff
+                mjd, h, m, s = timetag_to_mjdatetime(time_tag)
+                ### The actual save
+                outname = os.path.join(self.output_dir, str(mjd))
+                if not os.path.exists(outname):
+                    os.mkdir(outname)
+                filename = 'CorrMatrix_%i_%02i%02i%02i.npz' % (mjd, h, m, s)
+                outname = os.path.join(outname, filename)
+                numpy.savez(outname, data=corr)
+                self.log.debug("Wrote correlation matrix %i to disk as '%s'", intCount, os.path.basename(outname))
+
+                time_tag += navg * (fS / 100.0)
+                intCount += 1
+
+                curr_time = time.time()
+                process_time = curr_time - prev_time
+                self.log.debug("Matrix plotter processing time was %.3f s", process_time)
+                prev_time = curr_time
+                self.perf_proclog.update({'acquire_time': acquire_time,
+                                          'reserve_time': 0.0,
+                                          'process_time': process_time,})
+
+        self.log.info("MatrixOp - Done")
+
 class ImagingOp(object):
     def __init__(self, log, iring, oring, decimation=1, core=-1, gpu=-1):
         self.log = log
---
+++ plot_correlations.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+def _plot_matrices(correlations, averages):
+    #Build the plot.
+    pols = ['XX', 'XY', 'YX', 'YY']
+    fig = plt.figure(1) #constrained_layout=True)
+    axes = []
+    gs = gridspec.GridSpec(2,2)
+    for i in range(4):
+        gsp = gridspec.GridSpecFromSubplotSpec(2, 1,
+                subplot_spec=gs[i], wspace=0.0, hspace=0.0,
+                height_ratios=[1.0,0.3])
+        axes.append([])
+        axes[-1].append(fig.add_subplot(gsp[1]))
+        axes[-1].append(fig.add_subplot(gsp[0]))
+
+    fig.canvas.draw()
+
+    #Set the color scale.
+    vmin, vmax = np.percentile(np.abs(correlations), q=[1,99])
+
+    #Plot.
+    x = y = np.arange(correlations.shape[0]) + 1
+    mapper = {0: 'XX', 1: 'YX', 2: 'XY', 3: 'YY'}
+    for i in range(2):
+        for j in range(2):
+            indx = 2*i + j
+            ax = axes[indx][0]
+            #ax.cla()
+            ax.plot(x, averages[:,i,j], 'o')
+            if indx > 1:
+                ax.set_xlabel('Antenna Number', fontsize=12)
+            ax.set_ylabel('Mean', fontsize=12)
+            ax.tick_params(which='both', direction='in', length=8, labelsize=12)
+
+            ax = axes[indx][1]
+            ax.set_title(mapper[indx],fontsize=14)
+            #ax.cla()
+            c=ax.imshow(correlations[:,:,i,j], aspect='auto', origin='lower', interpolation='nearest', vmin=vmin, vmax=vmax, extent=(x.min(),x.max(),y.min(),y.max()))
+            ax.set_ylabel('Antenna Number', fontsize=12)
+            ax.set_xticks([])
+            ax.tick_params(which='both', direction='in', length=8, labelsize=12)
+
+    cb = fig.colorbar(c, ax=axes)#, orientation='horizontal')
+    cb.set_label(r'$|C_{ij}|$', fontsize=12)
+
+    plt.show()  
+
+def main(args):
+
+    #Read in the data.
+    corr = np.load(args.file)['data']
+
+    #Compute the average for each antenna.
+    avg = np.mean(corr, axis=1)
+
+    _plot_matrices(corr, avg)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Plot the correlation matrices output by the Orville Wideband Imager',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file', type=str,
+            help='.NPZ file output by the MatrixOp module')
+
+    args = parser.parse_args()
+    main(args)
+

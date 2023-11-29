25b367a7d4ca25c1ae01fdba500c6e7d8c8a86ce
jaycedowell <jayce.dowell@gmail.com>
2023-11-29 12:00:35 -0700

    More generic plot title.

--- analyze_correlations.py
+++ analyze_correlations.py
@@ -46,7 +46,7 @@ List: {crossed + 1}
 def _plot_matrices(matrix, title=None, mask=None, susX=None, susY=None, crossed=None):
     #Build the plot.
     if title is None:
-        title = 'LWA-SV Correlation Matrices' 
+        title = 'Orville Correlation Matrices' 
         mapper = {0: 'XX', 1: 'XY', 2: 'YX', 3: 'YY'}
     else:
         title = title

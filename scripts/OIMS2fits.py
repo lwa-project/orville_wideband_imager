0d8611eba99fc093d87463c67dce65d24cd488c5
Sarah Chastain <schastain@gwu.edu>
2024-01-04 16:00:49 -0700

    fix OIMS2fits, add pbcorr

--- scripts/OIMS2fits.py
+++ scripts/OIMS2fits.py
@@ -1,12 +1,104 @@
-import ../OrvilleImageDB
+#!/usr/bin/env python
+
 from lsl.common.mcs import mjdmpm_to_datetime
+from lsl.common.paths import DATA as dataPath
+from lsl.misc import parser as aph
+from scipy.interpolate import interp1d
 from astropy.io import fits as astrofits
+from astropy.coordinates import AltAz, EarthLocation, SkyCoord
+from astropy.wcs import WCS
+from astropy.time import Time
 from datetime import datetime, timedelta
 import os
 import sys
 import numpy
 import argparse
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import OrvilleImageDB
 
+def calcbeamprops(az,alt,header,freq):
+
+    # az and alt need to be the same shape as the image we will correct
+
+    i = 0
+    beamDict = numpy.load(os.path.join(dataPath, 'lwa1-dipole-emp.npz'))
+    polarpatterns = []
+    for beamCoeff in (beamDict['fitX'], beamDict['fitY']):
+        alphaE = numpy.polyval(beamCoeff[0,0,:],freq )
+        betaE =  numpy.polyval(beamCoeff[0,1,:],freq )
+        gammaE = numpy.polyval(beamCoeff[0,2,:],freq )
+        deltaE = numpy.polyval(beamCoeff[0,3,:],freq )
+        alphaH = numpy.polyval(beamCoeff[1,0,:],freq )
+        betaH =  numpy.polyval(beamCoeff[1,1,:],freq )
+        gammaH = numpy.polyval(beamCoeff[1,2,:],freq )
+        deltaH = numpy.polyval(beamCoeff[1,3,:],freq )
+        corrFnc = None
+
+        def compute_beam_pattern(az, alt, corr=corrFnc):
+            zaR = numpy.pi/2 - alt*numpy.pi / 180.0
+            azR = az*numpy.pi / 180.0
+
+            c = 1.0
+            if corrFnc is not None:
+                c = corrFnc(alt*numpy.pi / 180.0)
+                c = numpy.where(numpy.isfinite(c), c, 1.0)
+
+            pE = (1-(2*zaR/numpy.pi)**alphaE)*numpy.cos(zaR)**betaE + gammaE*(2*zaR/numpy.pi)*numpy.cos(zaR)**deltaE
+            pH = (1-(2*zaR/numpy.pi)**alphaH)*numpy.cos(zaR)**betaH + gammaH*(2*zaR/numpy.pi)*numpy.cos(zaR)**deltaH
+
+            return c*numpy.sqrt((pE*numpy.cos(azR))**2 + (pH*numpy.sin(azR))**2)
+        # Calculate the beam
+        pattern = compute_beam_pattern(az, alt)
+        polarpatterns.append(pattern)
+        i += 1
+    beamDict.close()
+    return polarpatterns[0], polarpatterns[1]
+
+def pbcorroims(header,imSize,chan):
+    mjd = int(header['start_time'])
+    mpm = int((header['start_time'] - mjd)*86400.0*1000.0)
+    tInt = header['int_len']*86400.0
+    dateObs = mjdmpm_to_datetime(mjd, mpm)
+    x = numpy.arange(imSize) - 0.5
+    y = numpy.arange(imSize) - 0.5
+    x,y = numpy.meshgrid(x,y)
+    pScale = header['pixel_size']
+    sRad   = 360.0/pScale/numpy.pi / 2
+    crval1 = header['center_ra']*numpy.pi/180
+    crpix1 = imSize/2 + 1 + 0.5 * ((imSize+1)%2) 
+    cdelt1 = numpy.pi*(-360.0/(2*sRad)/numpy.pi)/180
+    crval2 = header['center_dec']*numpy.pi/180
+    crpix2 = imSize/2 + 1 + 0.5 * ((imSize+1)%2) 
+    cdelt2 = numpy.pi*(360.0/(2*sRad)/numpy.pi)/180
+    ra = ((crval1 + (x - crpix1)*cdelt1/(numpy.cos(crval2)))*180/numpy.pi) 
+    dec = (crval2 + cdelt2*(y-crpix2))*180/numpy.pi
+    # Make dec go between -90 and 90
+    # Adjust RA accordingly
+    decover = dec>90
+    decdiff = dec[decover] - 90
+    dec[decover] = dec[decover] - decdiff
+    ra[decover] +=180
+    decoverneg = dec<-90
+    decdiffneg = dec[decoverneg] + 90
+    dec[decoverneg] = dec[decoverneg] + decdiffneg
+    ra[decoverneg] +=180
+    ra = ra % 360
+    
+    sc = SkyCoord(ra,dec,unit='deg')
+    lwasv = EarthLocation.from_geodetic(-106.885664,34.348562, height=1475) 
+    time = Time(dateObs.strftime("%Y-%m-%dT%H:%M:%S"),format="isot")
+    aa = AltAz(location=lwasv, obstime=time)
+    myaltaz = sc.transform_to(aa)
+    alt = myaltaz.alt.deg
+    az = myaltaz.az.deg
+    # Keep alt between 0 and 90, adjust az accordingly
+    negalt = alt < 0
+    alt[negalt] += 90
+    az[negalt] + 180
+
+    freq = (int(header['start_freq'])  + ((int(chan)+1)*int(header['bandwidth'])/2))
+    XX,YY = calcbeamprops(az,alt,header,freq)
+    return XX,YY
 def main(args):
     for filename in args.filename:
         db = OrvilleImageDB.OrvilleImageDB(filename, 'r')
@@ -43,6 +135,10 @@ def main(args):
             invalid = numpy.where( ((x-imSize/2.0)**2 + (y-imSize/2.0)**2) > (sRad**2) )
             imdata[:,:,invalid[0], invalid[1]] = 0.0
             ext = imSize/(2*sRad)
+            if args.pbcorr:
+                for i in range(ints):
+                    XX,YY = pbcorroims(hdrlist[i],imSize,chan)
+                    imdata[i,0]/=((XX+YY)/2)
             
             ## Convert the start MJD into a datetime instance and then use
             ## that to come up with a stop time
@@ -113,6 +209,8 @@ if __name__ == "__main__":
                         help='filename to convert')
     parser.add_argument('-f', '--force', action='store_true',
                         help='force overwriting of FITS files')
+    parser.add_argument('-p', '--pbcorr', action='store_true',
+                        help='Perform primary beam correction on Stokes I')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='be verbose during the conversion')
     args = parser.parse_args()

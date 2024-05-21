8425a41a0fc2e5e69407f53a035f77a6d2140e94
jaycedowell <jayce.dowell@gmail.com>
2024-05-21 16:13:45 -0600

    OrvilleImager -> OrvilleImage

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ import os
 import glob
 import shutil
 
-from setuptools import setup, Extension, Distribution, find_packages
+from setuptools import setup, find_namespace_packages
 
 def update_bad_freq():
     setup_script_path = os.path.dirname(os.path.abspath(__file__))
     bad_freq = os.path.join(setup_script_path, 'bad_freq.txt')
     if os.path.exists(bad_freq):
-        bad_freq_toolkit_path = os.path.join(setup_script_path, 'lsl_toolkits', 'OrvilleImager')
+        bad_freq_toolkit_path = os.path.join(setup_script_path, 'src', 'lsl_toolkits', 'OrvilleImage')
         shutil.copy(bad_freq, os.path.join(bad_freq_toolkit_path, '_bad_freq.py'))
 
-print('cat')
 update_bad_freq()
 
 setup(name                 = "lsl-toolkits-orvilleimage",
@@ -21,8 +20,8 @@ setup(name                 = "lsl-toolkits-orvilleimage",
       author               = "J. Dowell, S. Varghese, and G. B. Taylor",
       author_email         = "jdowell@unm.edu",
       license              = 'BSD3',
-      packages             = find_packages(exclude="tests"),
-      namespace_packages   = ['lsl_toolkits',],
+      packages             = find_namespace_packages(where='src/', include=['lsl_toolkits.OrvilleImage']),
+      package_dir          = {'': 'src'},
       scripts              = glob.glob('scripts/*.py'),
       python_requires      = '>=3.6',
       install_requires     = ['numpy', 'scipy', 'astropy', 'lsl'],

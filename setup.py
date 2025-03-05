6f1d68045459794dde9ccc3ec5b0974d644396e0
jaycedowell <jayce.dowell@gmail.com>
2025-03-04 18:57:32 -0700

    Update setup.py.

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ import shutil
 from setuptools import setup, find_namespace_packages
 
 setup(name                 = "lsl-toolkits-orvilleimage",
-      version              = "0.5.0",
+      version              = "0.6.0",
       description          = "LSL Toolkit for Orville Image Database Files",
       long_description     = "LWA Software Library reader for Orville Image Database files",
       author               = "J. Dowell, S. Varghese, and G. B. Taylor",
       author_email         = "jdowell@unm.edu",
       license              = 'BSD3',
-      packages             = find_namespace_packages(where='src/', include=['lsl_toolkits.OrvilleImage']),
+      packages             = find_namespace_packages(where='src/', include=['lsl_toolkits.OrvilleImage',
+                                                                            'lsl_toolkits.OrvilleImage.legacy'
+                                                                           ]),
       package_dir          = {'': 'src'},
       package_data         = {'lsl_toolkits.OrvilleImage': ['data/*.txt']},
       scripts              = glob.glob('scripts/*.py'),
